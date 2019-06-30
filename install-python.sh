d3c2f7370b5e219916e5d9868b7c9f09d226a044
imhcg <imhcg@qq.com>
2019-06-30 12:28:33 +0800

    install python

---
+++ install-python.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "install dependences"
+yum -y groupinstall "Development tools"
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
+
+echo "download and compile python"
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+tar -xvJf  Python-3.7.3.tar.xz
+cd Python-3.7.3
+./configure --prefix=/usr/local/bin/python3.7
+make
+make install
+
+echo "link python bin to app path"
+ln -s /usr/local/bin/python3.7/bin/python3.7 /usr/bin/python3.7
+ln -s /usr/local/bin/python3.7/bin/pip3.7 /usr/bin/pip3.7
