f8ec3118979e6bf18139a71fc2de99c9be18d37e
imhcg <imhcg@qq.com>
2019-07-12 11:16:05 +0800

    增加卸载

--- setup_flask_app_manager.py
+++ setup_flask_app_manager.py
@@ -2,7 +2,7 @@
 import os
 
 
-def main():
+def install():
     if not os.path.isdir("/www"):
         os.makedirs("/www")
 
@@ -16,6 +16,18 @@ def main():
         # 终止
         print("failed to install caddyserver")
         return
+
+    # 检查 python3.7 是否已经被安装
+    if not os.path.isfile("/usr/bin/python3.7"):
+        # 若没有，则执行安装脚本
+        os.system("curl https://raw.githubusercontent.com/hechenggang/i_love_linux/master/install-python.sh | bash install-python.sh")
+    
+    # 再次检查
+    if not os.path.isfile("/usr/bin/python3.7"):
+        # 终止
+        print("failed to install python3.7")
+        return
+    
 
     app_dir = "/www/flask_app_manager"
     
@@ -32,7 +44,53 @@ def main():
             os.path.join(app_dir, "requirements.txt")
         ))
     
+    # 创建 caddy 进程，两个（一个为主程序服务，一个为平台上的应用程序服务）
+    # 创建 flask_app_manager 进程
+    os.system("cp {}/config_file/daemon/caddy/caddy_for_app.service /etc/systemd/system/caddy_for_app.service".format(app_dir))
+    os.system("cp {}/config_file/daemon/caddy/caddy_for_flask_app_manager.service /etc/systemd/system/caddy_for_flask_app_manager.service".format(app_dir))
+    os.system("cp {}/config_file/daemon/flask_app_manager/flask_app_manager.service /etc/systemd/system/flask_app_manager.service".format(app_dir))
+    os.system("systemctl daemon-reload")
+    os.system("systemctl start flask_app_manager && systemctl enable flask_app_manager")
+    os.system("systemctl start caddy_for_flask_app_manager && systemctl enable caddy_for_flask_app_manager")
+    os.system("systemctl start caddy_for_app && systemctl enable caddy_for_app")
+    
+
     print("finish")
+
+
+def uninstall():
+    app_dir = "/www/flask_app_manager"
+    os.system("rm -rf {}".format(app_dir))
+    if not os.path.isdir(app_dir):
+        os.makedirs(app_dir)
+    d1 = "/etc/systemd/system/caddy_for_app.service"
+    d2 = "/etc/systemd/system/caddy_for_flask_app_manager.service"
+    d3 = "/etc/systemd/system/flask_app_manager.service"
+    for i in [d1,d2,d3]:
+        if os.path.isfile(i):
+            os.system("rm -rf {}".format(i))
+            name = os.path.split(i)[1].replace(".service","")
+            os.system("systemctl start {} && systemctl enable {}".format(name,name))
+
+    print("finish")
+
+def main():
+    print("1 INSTALL")
+    print("2 UNINSTALL")
+    a = int(input("INPUT NUMBER OF CHOOSE: "))
+    print(a)
+    if a == 1:
+        c = input("DO YOU WANT TO INSTALL ? (Y/N)")
+        if c == "Y":
+            install()
+        else:
+            print("CANCELED")
+    elif a == 2:
+        c = input("DO YOU WANT TO UNINSTALL ? (Y/N)")
+        if c == "Y":
+            uninstall()
+        else:
+            print("CANCELED")
 
 if __name__ == "__main__":
     main()
